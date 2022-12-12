a637e1c0602aef04acea59663a13c17b7dc2a948
dibbadan <daniele.dibattista@studenti.polito.it>
2022-12-12 15:28:55 +0100

    Small fix

--- src/actions/inspect.py
+++ src/actions/inspect.py
@@ -6,8 +6,10 @@ def select_artifact_and_inspect(api_key):
     filename = fd.askopenfilename()
     process = subprocess.run(['cas.exe', '--api-key', api_key, 'inspect', filename], check=True, stdout=subprocess.PIPE, universal_newlines=True)   
     output = process.stdout
-    encoded_signerID = output[727:output.index("Apikey")]
-    if encoded_signerID != '':
-        signerID = base64.b64decode(encoded_signerID).decode()
-        print("\nLast notarization performed by signerID: ", signerID)
+
+    if 'Apikey' in output:
+        encoded_signerID = output[727:output.index("Apikey")]
+        if encoded_signerID != '':
+            signerID = base64.b64decode(encoded_signerID).decode()
+            print("\nLast notarization performed by signerID: ", signerID)
     print(output)
